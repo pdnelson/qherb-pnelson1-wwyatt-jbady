c3201a3cce59171a5c40d0695ffb1acfb28662a8
wwyattycp <48131322+wwyattycp@users.noreply.github.com>
2019-03-24 17:45:55 -0400

    Add files via upload

---
+++ EntreLink/src/edu/ycp/cs320/entrelink/servlet/SearchServlet.java
@@ -0,0 +1,21 @@
+package edu.ycp.cs320.entrelink.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Project Servlet: doGet");
+		
+		req.getRequestDispatcher("/_view/search.jsp").forward(req, resp);
+	}	
+}
